ca6c3617a7e088b0a11cd26d29db0d30faf2e876
Curly_Kayl <endwarfor@gmail.com>
2023-01-18 00:11:15 +0300

    Exercise 13 completed.

--- Exercise13/Program.cs
+++ Exercise13/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+string numberStr = Console.ReadLine();
+int length = numberStr.Length;
+
+if (length >= 3)
+{
+	Console.WriteLine(numberStr[2]);
+}
+
+else
+{
+	Console.WriteLine("Третьей цифры нет.");
+}
