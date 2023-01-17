2e6d90bfbeb4428402de56a591b0017365475bcc
Curly_Kayl <endwarfor@gmail.com>
2023-01-17 22:23:43 +0300

    Exercise10 completed

--- Exercise10/Program.cs
+++ Exercise10/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.Clear();
+
+Console.Write("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int number2 = (number % 100) / 10;
+Console.WriteLine($"Вторая цифра числа {number} - {number2}.");
